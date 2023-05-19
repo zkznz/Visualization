8869697d6271821fe9c6f86ebe60619a1163a358
lzk <1174633615@qq.com>
2023-05-20 00:28:48 +0800

    前端初始化

---
+++ vision/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import echarts from 'echarts'
+
+Vue.config.productionTip = false
+Vue.prototype.$http = axios
+Vue.prototype.$echarts = echarts
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
