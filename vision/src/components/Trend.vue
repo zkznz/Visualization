1a9600a88962a2579b684c329565b72c8a538af8
lzk <1174633615@qq.com>
2023-05-20 22:07:27 +0800

    完成折线图样式

--- vision/src/components/Trend.vue
+++ vision/src/components/Trend.vue
@@ -24,13 +24,39 @@ export default {
     },
     methods: {
         initChart() {
-            this.chart = this.$echarts.init(this.$refs.chart);
+            this.chart = this.$echarts.init(this.$refs.chart, 'dark');
             let initOption = {
+                title: {
+                    text: '▎地区销量趋势',
+                    textStyle: {
+                        fontSize: 60,
+                    },
+                    top: 40,
+                    left: 20
+                },
                 xAxis: {
-                    type: 'category'
+                    type: 'category',
+                    boundaryGap: false
                 },
                 yAxis: {
                     type: 'value'
+                },
+                legend: {
+                    left: 20,
+                    top: '15%',
+                    icon: 'circle',
+                    itemWidth: 50,
+                    itemHeight: 50
+                },
+                grid: {
+                    left: '3%',
+                    top: '35%',
+                    right: '4%',
+                    bottom: '1%',
+                    containLabel: true
+                },
+                tooltip: {
+                    trigger: 'axis',
                 }
             }
             this.chart.setOption(initOption);
@@ -48,13 +74,29 @@ export default {
                     type: 'line',
                     name: item.name,
                     data: item.data,
-                    stack: 'map'
+                    stack: 'map',
+                    smooth: 'true'
                 }
             })
             let legendArr = valueArr.map(item => {
                 return item.name
             });
-            console.log(legendArr);
+            // 半透明的颜色值
+            const colorArr1 = [
+                'rgba(11, 168, 44, 0.5)',
+                'rgba(44, 110, 255, 0.5)',
+                'rgba(22, 242, 217, 0.5)',
+                'rgba(254, 33, 30, 0.5)',
+                'rgba(250, 105, 0, 0.5)'
+            ]
+            // 全透明的颜色值
+            const colorArr2 = [
+                'rgba(11, 168, 44, 0)',
+                'rgba(44, 110, 255, 0)',
+                'rgba(22, 242, 217, 0)',
+                'rgba(254, 33, 30, 0)',
+                'rgba(250, 105, 0, 0)'
+            ]
             let dataOption = {
                 xAxis: {
                     data: timeArr
