f9c183323853ba5fc62f70166b81a1707db4e871
lzk <1174633615@qq.com>
2023-05-22 09:27:01 +0800

    实现websocket发送数据

--- koa_server/service/websocket_service.js
+++ koa_server/service/websocket_service.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
-
+const path = require('path');
+const fileUtils = require('../utils/file_utils')
 const wss = new WebSocket.Server({
     port: 9998
 });
@@ -8,8 +9,24 @@ module.exports.listen = () => {
     wss.on('connection', client => {
         console.log('有客户端连接');
         //监听客户端发送的消息
-        client.on('message', msg => {
-
+        client.on('message', async msg => {
+            let payLoad = JSON.parse(msg);
+            let action = payLoad.action;
+            //需要返回图表JSON数据
+            if (action === 'getData') {
+                let filePath = '../data/' + payLoad.chartName + '.json';
+                filePath = path.join(__dirname, filePath);
+                //获取文件数据
+                let res = await fileUtils.getFileJsonData(filePath);
+                payLoad.data = res;
+                client.send(JSON.stringify(payLoad));
+            }
+            //将图表配置发送给每个连接的客户端
+            else {
+                wss.clients.forEach(client => {
+                    client.send(JSON.stringify(msg));
+                })
+            }
         });
 
     })
